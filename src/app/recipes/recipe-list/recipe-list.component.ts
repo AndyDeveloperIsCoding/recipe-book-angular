341c6e78d0d911eaf06358bb637f6391db5a8ce8
Sunny User <abc@example.com>
2022-12-02 07:14:36 +0200

    Main commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model'
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a test', 'https://images.unsplash.com/photo-1466637574441-749b8f19452f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmVjaXBlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60'),
+    new Recipe('A test recipe', 'This is simply a test', 'https://images.unsplash.com/photo-1506368249639-73a05d6f6488?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmVjaXBlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60')
+  ];
+}
